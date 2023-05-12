e4bb6aae3fbb84409309f087b9e3147279345fc5
loana <loan.quillet@gmail.com>
2023-05-12 17:04:53 +0200

    docs: Ajout de la documentation dans les classes Joueur, Quete et Coordonable.

--- src/module/Joueur.java
+++ src/module/Joueur.java
@@ -3,6 +3,9 @@ package module;
 public class Joueur extends Coordonable{
     private int exp;
 
+    /**
+     * Constructeur de la classe Joueur ne prenant aucun paramètre.
+     */
     public Joueur(){
         super(new int[] {0, 0});
         exp = 0;
@@ -12,10 +15,18 @@ public class Joueur extends Coordonable{
         return exp;
     }
 
+    /**
+     * Mets à jour les coordonnées du joueur à celles données en paramètre.
+     * @param parCoord int[2] – les coordonnées
+     */
     public void deplacer(int[] parCoord){
         coord = parCoord;
     }
 
+    /**
+     * Ajoute de l'expérience au joueur.
+     * @param expToAdd int – Le montant d'expérience à ajouter au joueur
+     */
     public void addExp(int expToAdd){
         exp = exp + expToAdd;
     }
--- src/module/Quete.java
+++ src/module/Quete.java
@@ -7,6 +7,17 @@ public class Quete extends Coordonable {
     private final String intitule;
     private boolean validee;
 
+    /**
+     * Constructeur de la classe quête, prenant en paramètre toutes les informations de quêtes contenues dans les
+     * fichiers scénarios, à l'exception du numéro de quête. (celui-ci est uniquement utilisé par la classe Scénario)
+     *
+     * @param parCoord int[2] – Les coordonnées de la quête
+     * @param parCond int[2][2] – Les préconditions de la quête
+     * @param parDuree int – La durée de la quête
+     * @param parExp int – L'expérience donnée par la quête. Si l'identifiant est 0 dans la classe scénario, cette
+     *               valeur sert de cap d'expérience à avoir pour battre la quête
+     * @param parIntitule String – L'intitulé de la quête
+     */
     public Quete(int[] parCoord, int[][] parCond, int parDuree, int parExp, String parIntitule){
         super(parCoord);
         cond = parCond;
