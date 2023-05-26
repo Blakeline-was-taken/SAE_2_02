c89c798399c7f0a935ec10c9b92b49a0159a9dae
loana <loan.quillet@gmail.com>
2023-05-26 10:36:52 +0200

    test: Ajout d'un test pour la méthode getQuete de la classe Scenario

--- src/tests/ScenarioTest.java
+++ src/tests/ScenarioTest.java
@@ -25,12 +25,7 @@ class ScenarioTest {
 
         TreeMap <Integer, Quete> mapTest = scnTest.getQuetes();
         for (int i = 0; i <= 4; i++){
-            Quete quest1 = mapTest.get(i);
-            Quete quest2 = mapTree.get(i);
-            assertEquals(0, quest1.distance(quest2));
-            assertEquals(quest1.getDuree(), quest2.getDuree());
-            assertEquals(quest1.getExp(), quest2.getExp());
-            assertEquals(quest1.getIntitule(), quest2.getIntitule());
+            assertEquals(mapTest.get(i), mapTree.get(i));
         }
 
         //Tests du champ successeurs :
@@ -55,6 +50,20 @@ class ScenarioTest {
 
         for (int key : voisinsTestTree.keySet()){
             assertEquals(voisinsTestTree.get(key), scnTest.getSuccesseurs().get(key));
+        }
+    }
+
+    @Test
+    public void getQuete() throws FileNotFoundException {
+        Scenario scnTest = new Scenario("scenario_0.txt");
+        TreeMap <Integer, Quete> mapTree = new TreeMap<>();
+        mapTree.put(0, new Quete(new int[] {1, 1}, new int[][] {{3, 4}, {0, 0}}, 4, 350, "vaincre Araignée lunaire"));
+        mapTree.put(1, new Quete(new int[] {4, 3}, new int[][] {{0, 0}, {0, 0}}, 2, 100, "explorer pic de Bhanborim"));
+        mapTree.put(2, new Quete(new int[] {3, 1}, new int[][] {{1, 0}, {0, 0}}, 1, 150, "dialoguer avec Kaela la chaman des esprits"));
+        mapTree.put(3, new Quete(new int[] {0, 4}, new int[][] {{2, 0}, {0, 0}}, 3, 200, "explorer palais de Ahehona"));
+        mapTree.put(4, new Quete(new int[] {3, 2}, new int[][] {{2, 0}, {0, 0}}, 6, 100, "vaincre Loup Géant"));
+        for (int queteId : mapTree.keySet()){
+            assertEquals(mapTree.get(queteId),scnTest.getQuete(queteId));
         }
     }
 }
