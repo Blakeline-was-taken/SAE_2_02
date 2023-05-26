cec670d1e51366c8faa297e7c0664ba026f73e54
loana <loan.quillet@gmail.com>
2023-05-26 09:00:00 +0200

    test: Correction du test du constructeur de Scenario

--- src/tests/ScenarioTest.java
+++ src/tests/ScenarioTest.java
@@ -14,11 +14,11 @@ class ScenarioTest {
     public void constructeur() throws FileNotFoundException {
         Scenario scnTest = new Scenario("scenario_0.txt");
         TreeMap <Integer, Quete> mapTree = new TreeMap<>();
-        mapTree.put(0, new Quete(new int[] {1, 1}, new int[][] {{3, 4}}, 4, 350, "vaincre Araignée lunaire"));
-        mapTree.put(1, new Quete(new int[] {4, 3}, new int[][] {}, 2, 100, "explorer pic de Bhanborin"));
-        mapTree.put(2, new Quete(new int[] {3, 1}, new int[][] {{1}, {}}, 1, 150, "dialoguer avec Kaela la chaman des esprits"));
-        mapTree.put(3, new Quete(new int[] {0, 4}, new int[][] {{2}}, 3, 200, "explorer palais de Ahehona"));
-        mapTree.put(4, new Quete(new int[] {3, 2}, new int[][] {{2}, {}}, 6, 100, "vaincre Loup Géant"));
+        mapTree.put(0, new Quete(new int[] {1, 1}, new int[][] {{3, 4}, {0, 0}}, 4, 350, "vaincre Araignée lunaire"));
+        mapTree.put(1, new Quete(new int[] {4, 3}, new int[][] {{0, 0}, {0, 0}}, 2, 100, "explorer pic de Bhanborin"));
+        mapTree.put(2, new Quete(new int[] {3, 1}, new int[][] {{1, 0}, {0, 0}}, 1, 150, "dialoguer avec Kaela la chaman des esprits"));
+        mapTree.put(3, new Quete(new int[] {0, 4}, new int[][] {{2, 0}, {0, 0}}, 3, 200, "explorer palais de Ahehona"));
+        mapTree.put(4, new Quete(new int[] {3, 2}, new int[][] {{2, 0}, {0, 0}}, 6, 100, "vaincre Loup Géant"));
 
         TreeMap <Integer, Quete> mapTest = scnTest.getQuetes();
         for (int i = 0; i <= 4; i++){
