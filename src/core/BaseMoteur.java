0c1190a7561394fbc9d555b8450a3ff076651dc1
ThomasIUT <totocraft28@gmail.com>
2023-06-01 12:05:19 +0200

    feat : Ajout des bases des classes BaseMoteur et Glouton

---
+++ src/core/BaseMoteur.java
@@ -0,0 +1,33 @@
+package core;
+
+import module.Joueur;
+import module.Quete;
+import module.Scenario;
+
+public abstract class BaseMoteur {
+
+    protected final Joueur joueur;
+    protected final Scenario scenario;
+
+    protected BaseMoteur(int idScenario){
+        
+    }
+
+    public int queteLaPlusProche(){
+        return 0;
+    }
+
+    public abstract int[] run(boolean isEfficace);
+
+    protected abstract int getNextEfficace();
+
+    protected abstract int getNextExhaustif();
+
+    public Joueur getJoueur(){
+        return joueur;
+    }
+
+    public Scenario getScenario(){
+        return scenario;
+    }
+}
---
+++ src/core/Glouton.java
@@ -0,0 +1,23 @@
+package core;
+
+public class Glouton extends BaseMoteur{
+
+    public Glouton(){
+        super(0); // TODO : à modifier
+    }
+
+    @Override
+    public int[] run(boolean isEfficace) {
+        return new int[0];
+    }
+
+    @Override
+    protected int getNextEfficace() {
+        return 0;
+    }
+
+    @Override
+    protected int getNextExhaustif() {
+        return 0;
+    }
+}
