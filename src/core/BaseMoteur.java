39b39f9b40bb12d11f2b9f72dffbc90bb0622811
ThomasIUT <totocraft28@gmail.com>
2023-06-01 13:32:50 +0200

    feat : ajout des méthodes de constructeur et quete la plus proche dans la classe Glouton

--- src/core/BaseMoteur.java
+++ src/core/BaseMoteur.java
@@ -4,13 +4,18 @@ import module.Joueur;
 import module.Quete;
 import module.Scenario;
 
+import java.io.FileNotFoundException;
+import java.net.IDN;
+import java.util.TreeSet;
+
 public abstract class BaseMoteur {
 
     protected final Joueur joueur;
     protected final Scenario scenario;
 
-    protected BaseMoteur(int idScenario){
-
+    protected BaseMoteur(int idScenario) throws FileNotFoundException {
+        joueur = new Joueur();
+        scenario = new Scenario("scenario_"+idScenario+".txt");
     }
 
     public int queteLaPlusProche(){
