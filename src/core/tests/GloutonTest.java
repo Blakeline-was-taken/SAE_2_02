c5993ea41322c42d510c8dfff0e839990443ea12
ThomasIUT <totocraft28@gmail.com>
2023-06-01 12:44:24 +0200

    test : Ajout des tests des méthosed constructeur et quete la plus proche de la classe BaseMoteur (via la classe Glouton)

---
+++ src/core/tests/GloutonTest.java
@@ -0,0 +1,49 @@
+package core.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+import core.Glouton;
+import module.Joueur;
+import module.Scenario;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+
+class GloutonTest {
+
+    @Test
+    public void constructeur() throws FileNotFoundException {
+        Glouton glTest = new Glouton(0);
+        // Vérification de la création du Joueur
+        Joueur jTest = glTest.getJoueur();
+        assertEquals(new int[] {0, 0}, jTest.getCoord());
+        assertEquals(0, jTest.getExp());
+        
+        //Vérification de la création du Scenario
+        Scenario scnTest = glTest.getScenario();
+        Scenario scnCheck = new Scenario("scenario_0.txt");
+        for (int idQuete : scnTest.getQuetes().keySet()){
+            assertEquals(scnCheck.getQuete(idQuete), scnTest.getQuete(idQuete));
+        }
+    }
+
+    @Test
+    public void queteLaPlusProche(){
+        Glouton glTest = new Glouton(0);
+        Scenario scTest = glTest.getScenario();
+        Joueur jTest = glTest.getJoueur();
+
+        assertEquals(1, glTest.queteLaPlusProche());
+        jTest.deplacer(scTest.getQuete(glTest.queteLaPlusProche()));
+        scTest.validerQuete(1);
+
+        assertEquals(2, glTest.queteLaPlusProche());
+        jTest.deplacer(scTest.getQuete(glTest.queteLaPlusProche()));
+        scTest.validerQuete(2);
+
+        assertEquals(4, glTest.queteLaPlusProche());
+        jTest.deplacer(scTest.getQuete(glTest.queteLaPlusProche()));
+        scTest.validerQuete(4);
+
+        assertEquals(0, glTest.queteLaPlusProche());
+    }
+}
