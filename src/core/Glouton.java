232b3697f72f60f42ebabf7c24edf94172129e26
ThomasIUT <totocraft28@gmail.com>
2023-06-01 15:42:31 +0200

    feat : Implémentation de toutes les méthodes de la classe Glouton

--- src/core/Glouton.java
+++ src/core/Glouton.java
@@ -1,23 +1,49 @@
 package core;
 
+import module.Quete;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
 public class Glouton extends BaseMoteur{
 
-    public Glouton(){
-        super(0); // TODO : à modifier
+    public Glouton(int idScenario) throws FileNotFoundException {
+        super(idScenario);
     }
 
     @Override
-    public int[] run(boolean isEfficace) {
-        return new int[0];
+    public ArrayList <Integer> run(boolean isEfficace) {
+        ArrayList <Integer> solution = new ArrayList<>();
+        while (!scenario.getQuetesValidees().contains(0)){
+            int current;
+            if (isEfficace){
+                current = getNextEfficace();
+            } else {
+                current = getNextExhaustif();
+            }
+            joueur.deplacer(scenario.getQuete(current));
+            joueur.addExp(scenario.getQuete(current).getExp());
+            scenario.validerQuete(current);
+            solution.add(current);
+        }
+        return solution;
     }
 
     @Override
     protected int getNextEfficace() {
-        return 0;
+        if (scenario.getAccessibleQuetes().contains(0) && joueur.getExp() >= scenario.getQuete(0).getExp()){
+            return 0;
+        } else {
+            return queteLaPlusProche();
+        }
     }
 
     @Override
     protected int getNextExhaustif() {
-        return 0;
+        if (scenario.getAccessibleQuetes().contains(0) && scenario.getAccessibleQuetes().size() == 1){
+            return 0;
+        } else {
+            return queteLaPlusProche();
+        }
     }
 }
