b518853e05628acad48b11a20358b5809ab7ce15
Thomas <totocraft28@gmail.com>
2023-06-06 16:18:48 +0200

    feat: Preparation de la classe controleur pour les boutons de la selection du scenario

---
+++ src/main/java/controleur/ScenarioControleur.java
@@ -0,0 +1,11 @@
+package controleur;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+public class ScenarioControleur implements EventHandler<ActionEvent> {
+    @Override
+    public void handle(ActionEvent event){
+
+    }
+}
