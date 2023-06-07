eeaeacaa3a0a3f4292724fa56d65eb6948cb907e
guillaumequillet <bgachats@yahoo.fr>
2023-06-07 18:53:23 +0200

    test: Ajout du test de la capacité de la classe Speedrun à générer les pires solutions

--- src/core/tests/SpeedrunTest.java
+++ src/core/tests/SpeedrunTest.java
@@ -28,5 +28,19 @@ public class SpeedrunTest {
         assertEquals(Arrays.toString(new int[] {1, 2, 4, 3, 0}), resultExh[0].toString());
         assertEquals(Arrays.toString(new int[] {1, 2, 3, 4, 0}), resultExh[1].toString());
         assertNull(resultExh[2]);
+
+        spTest = new Speedrun(0, 10);
+        resultEff = spTest.run(true, false);
+        assertNull(resultEff[4]);
+        assertEquals(Arrays.toString(new int[] {1, 2, 4, 0}), resultEff[3].toString());
+        assertEquals(Arrays.toString(new int[] {1, 2, 3, 0}), resultEff[2].toString());
+        assertEquals(Arrays.toString(new int[] {1, 2, 4, 3, 0}), resultEff[1].toString());
+        assertEquals(Arrays.toString(new int[] {1, 2, 3, 4, 0}), resultEff[0].toString());
+
+        spTest = new Speedrun(0, 5);
+        resultExh = spTest.run(false, true);
+        assertNull(resultExh[2]);
+        assertEquals(Arrays.toString(new int[] {1, 2, 4, 3, 0}), resultExh[1].toString());
+        assertEquals(Arrays.toString(new int[] {1, 2, 3, 4, 0}), resultExh[0].toString());
     }
 }
